9e0b27e19eb1dd5367efe9df9034010fe217b5d6
Paprikar <40892869+Paprikar@users.noreply.github.com>
2018-11-23 10:29:20 +0300

    add imageai part

--- Raccoon Boxing/boxing.py
+++ Raccoon Boxing/boxing.py
@@ -3,11 +3,13 @@ def draw_boxes(img, boxes):
     
     for box in boxes:
         img = cv2.rectangle(img, (box['xmin'], box['ymin']), (box['xmax'], box['ymax']), (0, 255, 0), 2)
-        cv2.putText(img, 
-                    box['class']+' '+box['prob'] if 'brob' in box else box['class'], 
-                    (box['xmin'], box['ymin'] - 6), 
-                    cv2.FONT_ITALIC, 
-                    1.2e-3 * img.shape[0], 
+        cv2.fillPoly(img, (box['xmin'], box['ymin'], box['xmax'], box['ymax']), (0,255,0))
+        ann = box['class']+' '+str(box['prob'])[:5] if 'prob' in box else box['class']
+        cv2.putText(img,
+                    ann,
+                    (box['xmin'], box['ymin'] - 6),
+                    cv2.FONT_ITALIC,
+                    1.2e-3 * img.shape[0],
                     (0,255,0),
                     2,
                     cv2.LINE_AA)
